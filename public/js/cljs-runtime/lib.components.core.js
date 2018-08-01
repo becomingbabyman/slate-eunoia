12a1fb2edc551cd38ca15dfbf7facb1a26c5dca0
Chris Smothers <chris.smothers@gmail.com>
2018-08-01 17:54:02 -0400

    a bit more style

--- public/js/cljs-runtime/lib.components.core.js
+++ public/js/cljs-runtime/lib.components.core.js
@@ -5,45 +5,51 @@ var module$node_modules$react$index=shadow.js.require("module$node_modules$react
 goog.require('cljss.core');
 goog.require('cljss.reagent');
 (window["React"] = module$node_modules$react$index);
+lib.components.core.transparent = "rgba(0,0,0,0)";
+lib.components.core.black = "#1b2733";
+lib.components.core.blue = "#0070e0";
+lib.components.core.grey = "#637282";
+lib.components.core.mid_grey = "#e6e8eb";
+lib.components.core.light_grey = "#f7f9fa";
 lib.components.core.editor_style = (function lib$components$core$editor_style(){
-return cljss.core.css("lib_components_core__editor-style",new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [".lib_components_core__editor-style{padding:10px;}"], null),cljs.core.PersistentVector.EMPTY);
+return cljss.core.css("lib_components_core__editor-style",new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [".lib_components_core__editor-style{padding:10px;font-family:-apple-system, BlinkMacSystemFont, San Francisco, sans-serif;-webkit-font-smoothing:antialiased;-webkit-print-color-adjust:exact;color:var(--var-lib_components_core__editor-style-0);-webkit-tap-highlight-color:var(--var-lib_components_core__editor-style-1);}"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, ["--var-lib_components_core__editor-style-0",lib.components.core.black], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, ["--var-lib_components_core__editor-style-1",lib.components.core.transparent], null)], null));
 });
-lib.components.core.h1 = (function (){var G__30120 = "lib_components_core__h1";
-var G__30121 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [".lib_components_core__h1{font-weight:600;}"], null);
-var G__30122 = [];
-var G__30123 = [];
-var G__30124 = ((function (G__30120,G__30121,G__30122,G__30123){
+lib.components.core.h1 = (function (){var G__30838 = "lib_components_core__h1";
+var G__30839 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [".lib_components_core__h1{font-size:30px;line-height:36px;font-weight:600;word-spacing:1px;margin-top:30px;margin-bottom:12px;}"], null);
+var G__30840 = [];
+var G__30841 = [];
+var G__30842 = ((function (G__30838,G__30839,G__30840,G__30841){
 return (function (p1__29390__29392__auto__,p2__29391__29393__auto__){
 return cljs.core.apply.cljs$core$IFn$_invoke$arity$4(React.createElement,"h1",cljs.core.clj__GT_js(p1__29390__29392__auto__),cljs.core.map.cljs$core$IFn$_invoke$arity$2(reagent.core.as_element,p2__29391__29393__auto__));
-});})(G__30120,G__30121,G__30122,G__30123))
+});})(G__30838,G__30839,G__30840,G__30841))
 ;
-return (cljss.reagent.styled.cljs$core$IFn$_invoke$arity$5 ? cljss.reagent.styled.cljs$core$IFn$_invoke$arity$5(G__30120,G__30121,G__30122,G__30123,G__30124) : cljss.reagent.styled.call(null,G__30120,G__30121,G__30122,G__30123,G__30124));
+return (cljss.reagent.styled.cljs$core$IFn$_invoke$arity$5 ? cljss.reagent.styled.cljs$core$IFn$_invoke$arity$5(G__30838,G__30839,G__30840,G__30841,G__30842) : cljss.reagent.styled.call(null,G__30838,G__30839,G__30840,G__30841,G__30842));
 })();
 
 lib.components.core.h1.displayName = "lib.components.core.h1";
-lib.components.core.h2 = (function (){var G__30125 = "lib_components_core__h2";
-var G__30126 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [".lib_components_core__h2{font-weight:600;}"], null);
-var G__30127 = [];
-var G__30128 = [];
-var G__30129 = ((function (G__30125,G__30126,G__30127,G__30128){
+lib.components.core.h2 = (function (){var G__30843 = "lib_components_core__h2";
+var G__30844 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [".lib_components_core__h2{font-size:21px;line-height:28px;font-weight:600;word-spacing:0.5px;margin-top:18px;margin-bottom:6px;}"], null);
+var G__30845 = [];
+var G__30846 = [];
+var G__30847 = ((function (G__30843,G__30844,G__30845,G__30846){
 return (function (p1__29390__29392__auto__,p2__29391__29393__auto__){
 return cljs.core.apply.cljs$core$IFn$_invoke$arity$4(React.createElement,"h2",cljs.core.clj__GT_js(p1__29390__29392__auto__),cljs.core.map.cljs$core$IFn$_invoke$arity$2(reagent.core.as_element,p2__29391__29393__auto__));
-});})(G__30125,G__30126,G__30127,G__30128))
+});})(G__30843,G__30844,G__30845,G__30846))
 ;
-return (cljss.reagent.styled.cljs$core$IFn$_invoke$arity$5 ? cljss.reagent.styled.cljs$core$IFn$_invoke$arity$5(G__30125,G__30126,G__30127,G__30128,G__30129) : cljss.reagent.styled.call(null,G__30125,G__30126,G__30127,G__30128,G__30129));
+return (cljss.reagent.styled.cljs$core$IFn$_invoke$arity$5 ? cljss.reagent.styled.cljs$core$IFn$_invoke$arity$5(G__30843,G__30844,G__30845,G__30846,G__30847) : cljss.reagent.styled.call(null,G__30843,G__30844,G__30845,G__30846,G__30847));
 })();
 
 lib.components.core.h2.displayName = "lib.components.core.h2";
-lib.components.core.h3 = (function (){var G__30130 = "lib_components_core__h3";
-var G__30131 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [".lib_components_core__h3{font-weight:600;}"], null);
-var G__30132 = [];
-var G__30133 = [];
-var G__30134 = ((function (G__30130,G__30131,G__30132,G__30133){
+lib.components.core.h3 = (function (){var G__30848 = "lib_components_core__h3";
+var G__30849 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [".lib_components_core__h3{font-size:14px;line-height:22px;font-weight:700;margin-top:0px;margin-bottom:0px;}"], null);
+var G__30850 = [];
+var G__30851 = [];
+var G__30852 = ((function (G__30848,G__30849,G__30850,G__30851){
 return (function (p1__29390__29392__auto__,p2__29391__29393__auto__){
 return cljs.core.apply.cljs$core$IFn$_invoke$arity$4(React.createElement,"h3",cljs.core.clj__GT_js(p1__29390__29392__auto__),cljs.core.map.cljs$core$IFn$_invoke$arity$2(reagent.core.as_element,p2__29391__29393__auto__));
-});})(G__30130,G__30131,G__30132,G__30133))
+});})(G__30848,G__30849,G__30850,G__30851))
 ;
-return (cljss.reagent.styled.cljs$core$IFn$_invoke$arity$5 ? cljss.reagent.styled.cljs$core$IFn$_invoke$arity$5(G__30130,G__30131,G__30132,G__30133,G__30134) : cljss.reagent.styled.call(null,G__30130,G__30131,G__30132,G__30133,G__30134));
+return (cljss.reagent.styled.cljs$core$IFn$_invoke$arity$5 ? cljss.reagent.styled.cljs$core$IFn$_invoke$arity$5(G__30848,G__30849,G__30850,G__30851,G__30852) : cljss.reagent.styled.call(null,G__30848,G__30849,G__30850,G__30851,G__30852));
 })();
 
 lib.components.core.h3.displayName = "lib.components.core.h3";
--- src/lib/components/core.cljs
+++ src/lib/components/core.cljs
@@ -8,16 +8,38 @@
 ; https://github.com/roman01la/cljss/issues/45
 (aset js/window "React" react)
 
-; (css/inject-global
-;  {:body
-;   {:background "blue"}})
+(def transparent "rgba(0,0,0,0)")
+(def black "#1b2733")
+(def blue "#0070e0")
+(def grey "#637282")
+(def mid-grey "#e6e8eb")
+(def light-grey "#f7f9fa")
 
 (defstyles editor-style []
-           {:padding "10px"})
+           {:padding "10px"
+            :color black
+            :font-family "-apple-system, BlinkMacSystemFont, San Francisco, sans-serif"
+            :-webkit-font-smoothing "antialiased"
+            :-webkit-print-color-adjust "exact"
+            :-webkit-tap-highlight-color transparent})
 
 (defstyled h1 :h1
-           {:font-weight "600"})
+           {:font-size "30px"
+            :line-height "36px"
+            :font-weight "600"
+            :word-spacing "1px"
+            :margin-top "30px"
+            :margin-bottom "12px"})
 (defstyled h2 :h2
-           {:font-weight "600"})
+           {:font-size "21px"
+            :line-height "28px"
+            :font-weight "600"
+            :word-spacing "0.5px"
+            :margin-top "18px"
+            :margin-bottom "6px"})
 (defstyled h3 :h3
-           {:font-weight "600"})
+           {:font-size "14px"
+            :line-height "22px"
+            :font-weight "700"
+            :margin-top "0px"
+            :margin-bottom "0px"})
