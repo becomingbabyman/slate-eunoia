4fcb4b06c7df8f9764d83459e0dc1a361588542d
Chris Smothers <chris.smothers@gmail.com>
2018-08-07 15:34:51 -0700

    enable placeholder blockquote button

--- src/lib/placeholder.cljs
+++ src/lib/placeholder.cljs
@@ -1,6 +1,7 @@
 (ns lib.placeholder
   (:require [reagent.core :as r]
-            [lib.components.core :as c]))
+            [lib.components.core :as c]
+            [lib.plugins.features.blockquote :as blockquote]))
 
 (defn render-block-placeholder-bar?
   "Should the bar of icons that shows up in an empty focused paragraph be shown"
@@ -13,9 +14,25 @@
 
 (defn render-placeholder [props]
   (when (render-block-placeholder-bar? props)
+    (js/console.log props)
     (c/block-placeholder-bar
      {:content-editable false}
      (c/block-placeholder-button
+      {:title "TODO: embed a link"}
       (c/icon "youtube"))
      (c/block-placeholder-button
+      {:title "TODO: upload an image"}
+      (c/icon "image"))
+     (c/block-placeholder-button
+      {:title "TODO: make a list"}
+      (c/icon "list"))
+     (c/block-placeholder-button
+      {:title "TODO: make a check list"}
+      (c/icon "check-square"))
+     (c/block-placeholder-button
+      {:title "TODO: make a code block"}
+      (c/icon "code"))
+     (c/block-placeholder-button
+      {:title "TODO: REMOVE: make a blockquote"
+       :on-click #(.change props.editor blockquote/transform)}
       (c/icon "chevron-right")))))
