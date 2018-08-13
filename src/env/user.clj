58be2253c109a51138e27f6b1974c9427335840c
Chris Smothers <chris.smothers@gmail.com>
2018-08-13 19:07:03 -0400

    [CLN] add REPL instructions to readme

--- src/env/user.clj
+++ src/env/user.clj
@@ -2,8 +2,11 @@
 
 (defn reset [])
 
-; (defn nrepl-browser []
+; (defn nrepl-example []
 ;   (shadow.cljs.devtools.api/nrepl-select :example))
+;
+; (defn nrepl-test []
+;   (shadow.cljs.devtools.api/nrepl-select :browser-test))
 ;
 ; (defn nrepl-node []
 ;   (shadow.cljs.devtools.api/node-repl))
