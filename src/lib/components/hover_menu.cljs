55afede48048708a2246d9908f9d351345390e91
Chris Smothers <chris.smothers@gmail.com>
2018-08-19 19:07:19 -0400

    [ENH] UI to add URLs to selection

--- src/lib/components/hover_menu.cljs
+++ src/lib/components/hover_menu.cljs
@@ -1,29 +1,44 @@
 (ns lib.components.hover-menu
   (:require [reagent.core :as r]
             [lib.components.core :as c]
+            [cljss.core :refer-macros [defstyles]]
             [cljss.reagent :refer-macros [defstyled]]
             [lib.plugins.features.bold :as bold]
             [lib.plugins.features.strikethrough :as strikethrough]
             [lib.plugins.features.highlight :as highlight]
+            [lib.plugins.features.link :as link]
             [lib.plugins.features.header :as header]))
+
+(def menu-ref (atom nil))
+(def focused-on-link? (r/atom false))
+(def last-selection-style (atom {}))
+(def link-transform-handler (atom nil))
+(def url (r/atom ""))
 
 (defstyled menu :div
            {:position "absolute"
             :display "flex"
             :flex-direction "row"
+
             :opacity 0
             :left "-1000px"
             :top "-1000px"
             ; :opacity 1
             ; :left "100px"
             ; :top "100px"
+
             :margin-top "-6px"
-            :background c/black
-            :border (str "1px solid " c/pitch-black)
             :border-radius "4px"
             :transition "opacity 0.3s"
-            :white-space "nowrap"
+            :white-space "nowrap"})
+
+(defstyles menu-dark []
+           {:background c/black
             :box-shadow c/dark-box-shadow})
+
+(defstyles menu-light []
+           {:background c/white
+            :box-shadow c/light-box-shadow})
 
 (defstyled divider :span
            {:background c/pitch-black
@@ -43,6 +58,38 @@
            {:font-size "14px"
             :font-weight "700"})
 
+(defstyled input :input
+           {:font-size "14px"
+            :outline "none"
+            :padding-top "10px"
+            :padding-left "12px"
+            :padding-bottom "11px"
+            :box-sizing "border-box"
+            :width "300px"
+            :margin-right "38px"
+            :font-weight "400"
+            :height "100%"
+            :line-height "18px"
+            :vertical-align "top"
+            :background-color "transparent"
+            :border 0
+            :color c/blue
+            :transition "width 100ms ease-in"})
+
+(defstyled x-button :button
+           {:position "absolute"
+            :top 0
+            :right 0
+            :color c/black
+            :background "#e6e8eb !important"
+            :font-size "20px"
+            :line-height "20px"
+            :font-weight "500"
+            :height "38px"
+            :width "38px"
+            :padding "9px"
+            "&:hover" {:background "#c1c7cd !important"}})
+
 (defn icon [name]
   (c/icon name {:height 18 :width 18 :stroke-width 3}))
 
@@ -54,74 +101,116 @@
    child))
 
 (defn selection-style [menu]
-  (when menu
-    (let [rect (-> js/window
-                   (.getSelection)
-                   (.getRangeAt 0)
-                   (.getBoundingClientRect))
-          top (+ js/window.pageYOffset
-                 (- rect.top
-                    menu.offsetHeight))
-          left (min (- js/window.innerWidth menu.offsetWidth 5)
-                    (max 5
-                         (- (+ rect.left js/window.pageXOffset (/ rect.width  2))
-                            (/ menu.offsetWidth 2))))
-          style  {:opacity 1
-                  :top (str top "px")
-                  :left (str left "px")}]
-      (if (< top 11)
-        (merge style {:top (str (+ rect.bottom 11) "px")})
-        style))))
+  (if @focused-on-link?
+    @last-selection-style
+    (when menu
+      (let [rect (-> js/window
+                     (.getSelection)
+                     (.getRangeAt 0)
+                     (.getBoundingClientRect))
+            top (+ js/window.pageYOffset
+                   (- rect.top
+                      menu.offsetHeight))
+            left (min (- js/window.innerWidth menu.offsetWidth 5)
+                      (max 5
+                           (- (+ rect.left js/window.pageXOffset (/ rect.width  2))
+                              (/ menu.offsetWidth 2))))
+            style  {:opacity 1
+                    :top (str top "px")
+                    :left (str left "px")}
+            final-style (if (< top 11)
+                          (merge style {:top (str (+ rect.bottom 11) "px")})
+                          style)]
+        (reset! last-selection-style final-style)
+        final-style))))
 
 (defn active-mark? [value mark-type]
   (.some value.activeMarks #(= (aget %1 "type") mark-type)))
 
-(def ref (atom nil))
-
-(defn click-handler-handler [value on-change]
+(defn transform-handler-handler [value on-change]
   (fn [transform]
     (fn [event]
       (.preventDefault event)
-      (on-change (transform (.change value))))))
+      (-> (.change value) ; Get a new change object
+          (transform) ; Perform the curried transformation
+          (on-change))))) ; Makes sure to callback to the on-change handler of the editor to propagate the changes
+
+(defn focus-on-link [event transform-handler]
+  (.preventDefault event)
+  (reset! focused-on-link? true)
+  (reset! link-transform-handler transform-handler))
+
+(defn on-submit-url [event]
+  (.preventDefault event)
+  ((@link-transform-handler (link/transform @url)) event)
+  (reset! url ""))
+
+(defn remove-link [event]
+  (.preventDefault event)
+  (print "TODO: >>>>>>"))
 
 (defn hover-menu [props]
   (let [value (get props :value)
         blurred? value.isBlurred
         empty? (try value.isEmpty (catch :default e false))
-        active-style (if (or blurred? empty?)
-                       {} (selection-style @ref))
-        click-handler (click-handler-handler value (get props :on-change))]
-    (menu {:ref #(reset! ref %1)
+        link? (try (= "link" value.anchorInline.type) (catch :default e false))
+        active-style (if (or (and (not blurred?) (not empty?))
+                             @focused-on-link?)
+                       (selection-style @menu-ref) {})
+        transform-handler (transform-handler-handler value (get props :on-change))]
+
+    (js/console.log @focused-on-link? blurred? empty? link? props)
+
+    (menu {:ref #(reset! menu-ref %1)
+           :class (if @focused-on-link? (menu-light) (menu-dark))
            :style active-style}
-          (tooltip "Bold" "⌘ + B"
-                   (button
-                     {:on-mouse-down (click-handler bold/transform)
-                      :style {:color (when (active-mark? value "bold") c/white)}}
-                     (icon "bold")))
-          (tooltip "Strikethrough" "⌘ + Opt + S"
-                   (button
-                     {:on-mouse-down (click-handler strikethrough/transform)
-                      :style {:color (when (active-mark? value "strikethrough") c/white)}}
-                     (icon "minus")))
-          (tooltip "Highlight" "⌘ + E"
-                   (button
-                     {:on-mouse-down (click-handler highlight/transform)
-                      :style {:color (when (active-mark? value "highlight") c/white)}}
-                     (icon "edit-3")))
-          (tooltip "Add a Link" "⌘ + K"
-                   (button (icon "link")))
-          (divider)
-          (tooltip "Large Header" "# Space"
-                   (button
-                     {:on-mouse-down (click-handler (header/transform 1))
-                      :style {:color (when (= value.anchorBlock.type "header1") c/white)}}
-                     "H" (small "1")))
-          (tooltip "Small Header" "## Space"
-                   (button
-                     {:on-mouse-down (click-handler (header/transform 2))
-                      :style {:color (when (= value.anchorBlock.type "header2") c/white)}}
-                     (small "H2")))
-          ; (button (icon "list"))
-          ; (button (icon "check-square"))
-          (divider))))
-          ; (button (icon "message-square")))))
+          (if @focused-on-link?
+            [:<>
+             [:form {:on-submit on-submit-url}
+              (input {:type "url"
+                      :placeholder "Enter link URL"
+                      :auto-focus true
+                      :default-value @url
+                      :on-change (fn [e] (reset! url (.. e -target -value)))
+                      :on-focus #(reset! focused-on-link? true)
+                      :on-blur #(reset! focused-on-link? false)})]]
+             ; (x-button {:on-click remove-link}
+             ;           (icon "x"))]
+            [:<>
+             (tooltip "Bold" "⌘ + B"
+                      (button
+                        {:on-mouse-down (transform-handler bold/transform)
+                         :style {:color (when (active-mark? value "bold") c/white)}}
+                        (icon "bold")))
+             (tooltip "Strikethrough" "⌘ + Opt + S"
+                      (button
+                        {:on-mouse-down (transform-handler strikethrough/transform)
+                         :style {:color (when (active-mark? value "strikethrough") c/white)}}
+                        (icon "minus")))
+             (tooltip "Highlight" "⌘ + E"
+                      (button
+                        {:on-mouse-down (transform-handler highlight/transform)
+                         :style {:color (when (active-mark? value "highlight") c/white)}}
+                        (icon "edit-3")))
+             (tooltip "Create Link" "⌘ + K"
+                      (button
+                        {:on-mouse-down #(focus-on-link % transform-handler)}
+                        (icon "link")))
+             (divider)
+             (tooltip "Large Header" "# Space"
+                      (button
+                        {:on-mouse-down (transform-handler (header/transform 1))
+                         :style {:color (when (= value.anchorBlock.type "header1") c/white)}}
+                        "H" (small "1")))
+             (tooltip "Small Header" "## Space"
+                      (button
+                        {:on-mouse-down (transform-handler (header/transform 2))
+                         :style {:color (when (= value.anchorBlock.type "header2") c/white)}}
+                        (small "H2")))
+             (tooltip "TODO:" "..."
+                      (button (icon "list")))
+             (tooltip "TODO:" "..."
+                      (button (icon "check-square")))
+             (divider)
+             (tooltip "TODO:" "..."
+                      (button (icon "message-square")))]))))
--- src/lib/plugins/features/link.cljs
+++ src/lib/plugins/features/link.cljs
@@ -3,18 +3,38 @@
             [lib.components.core :as c]
             [lib.plugins.helpers.auto-replace :refer [auto-replace]]))
 
+(defn show-hover-menu [props]
+  (fn [event]
+    (.change props.editor
+             #(.setNodeByKey %
+                             props.node.key
+                             (clj->js
+                              {:data {:hovered true
+                                      :url "http://npr.org"}})))
+    (js/console.log "TODO: show link hover menu" props)))
+
+(defn hide-hover-menu [props]
+  (fn [event]
+    (js/console.log "TODO: hide link hover menu")))
+
+(defn transform [url]
+  (fn [change]
+    (.wrapInline change (clj->js {:type "link"
+                                  :data {:url url}}))))
+
 (defn render-node [props]
   (when (= "link" props.node.type)
+    (js/console.log props)
     (let [url (.get props.node.data "url")]
-      (c/tooltip-wrap ; TODO: see if the hover-menu can be used instead of rendering a hidden tooltip with every link.
-       (c/tooltip "TODO: ... " url)
-       (c/a
-        (merge
-         (js->clj props.attributes)
-         {:href url
-          :target "_blank"
-          :on-click #(.open js/window url "_blank")})
-        props.children)))))
+      ; (c/tooltip-wrap ; TODO: see if the hover-menu can be used instead of rendering a hidden tooltip with every link.
+      ;  (c/tooltip "TODO: ... " url)
+      (c/a (merge (js->clj props.attributes)
+                  {:href url
+                   :target "_blank"
+                   :on-click #(.open js/window url "_blank")})
+                   ; :on-mouse-over (show-hover-menu props)
+                   ; :on-mouse-out (hide-hover-menu props)})
+           props.children))))
 
 (defn link
   "Adds link support to editor"
